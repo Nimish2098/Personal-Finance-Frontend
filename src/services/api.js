9d06402d76b5149c13c87c6575f6aa9bab599055
Nimish2098 <raonimish23@gmail.com>
2026-01-14 00:46:00 +0530

    deployment ready:

--- src/services/api.js
+++ src/services/api.js
@@ -19,15 +19,12 @@ api.interceptors.request.use((config) => {
 })
 
 // Handle 401/403 responses
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401 || error.response?.status === 403) {
-      localStorage.removeItem("token")
-      window.location.href = "/login"
-    }
-    return Promise.reject(error)
-  },
-)
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token")
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
 
 export default api
